242bc2f156c59c0287fffb35251b42094e72de15
John Amores <kayak.stated_0b@icloud.com>
2025-02-11 21:45:42 -0500

    Added Dashboard Wrapper

---
+++ client/src/app/dashboardWrapper.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const DashboardWrapper = ({ children }: {children: React.ReactNode}) => {
+    return(
+        <div>DashboardWrapper {children} </div>
+    )
+}
+
+
+
+export default DashboardWrapper;
