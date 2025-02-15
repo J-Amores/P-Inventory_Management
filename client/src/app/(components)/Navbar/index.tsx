f418e94418f49501662b449a8c38b3323c7c2a0c
John Amores <kayak.stated_0b@icloud.com>
2025-02-15 18:51:17 -0500

    Removing state management logic

--- client/src/app/(components)/Navbar/index.tsx
+++ client/src/app/(components)/Navbar/index.tsx
@@ -1,26 +1,14 @@
 "use client";
 
-import { useAppDispatch, useAppSelector } from "@/app/redux";
-import { setIsDarkMode, setIsSidebarCollapsed } from "@/state";
+
+
 import { Bell, Menu, Moon, Settings, Sun } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
 const Navbar = () => {
-  const dispatch = useAppDispatch();
-  const isSidebarCollapsed = useAppSelector(
-    (state) => state.global.isSidebarCollapsed
-  );
-  const isDarkMode = useAppSelector((state) => state.global.isDarkMode);
 
-  const toggleSidebar = () => {
-    dispatch(setIsSidebarCollapsed(!isSidebarCollapsed));
-  };
-
-  const toggleDarkMode = () => {
-    dispatch(setIsDarkMode(!isDarkMode));
-  };
 
   return (
     <div className="flex justify-between items-center w-full mb-7">
@@ -28,7 +16,7 @@ const Navbar = () => {
       <div className="flex justify-between items-center gap-5">
         <button
           className="px-3 py-3 bg-gray-100 rounded-full hover:bg-blue-100"
-          onClick={toggleSidebar}
+          
         >
           <Menu className="w-4 h-4" />
         </button>
@@ -50,12 +38,8 @@ const Navbar = () => {
       <div className="flex justify-between items-center gap-5">
         <div className="hidden md:flex justify-between items-center gap-5">
           <div>
-            <button onClick={toggleDarkMode}>
-              {isDarkMode ? (
-                <Sun className="cursor-pointer text-gray-500" size={24} />
-              ) : (
-                <Moon className="cursor-pointer text-gray-500" size={24} />
-              )}
+            <button>
+              
             </button>
           </div>
           <div className="relative">
--- client/src/app/dashboardWrapper.tsx
+++ client/src/app/dashboardWrapper.tsx
@@ -2,7 +2,6 @@
 
 import React, { useEffect } from "react";
 import Navbar from "@/app/(components)/Navbar";
-import StoreProvider, { useAppSelector } from "./redux";
 import Sidebar from "./(components)/Sidebar";
 
 
@@ -10,30 +9,14 @@ import Sidebar from "./(components)/Sidebar";
 
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
-    const isSidebarCollapsed = useAppSelector(
-      (state) => state.global.isSidebarCollapsed
-    );
-    const isDarkMode = useAppSelector((state) => state.global.isDarkMode);
-  
-    useEffect(() => {
-      if (isDarkMode) {
-        document.documentElement.classList.add("dark");
-      } else {
-        document.documentElement.classList.add("light");
-      }
-    });
+
+
   
     return (
       <div
-        className={`${
-          isDarkMode ? "dark" : "light"
-        } flex bg-gray-50 text-gray-900 w-full min-h-screen`}
       >
         <Sidebar />
         <main
-          className={`flex flex-col w-full h-full py-7 px-9 bg-gray-50 ${
-            isSidebarCollapsed ? "md:pl-24" : "md:pl-72"
-          }`}
         >
           <Navbar />
           {children}
@@ -44,9 +27,8 @@ const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   
   const DashboardWrapper = ({ children }: { children: React.ReactNode }) => {
     return (
-      <StoreProvider>
         <DashboardLayout>{children}</DashboardLayout>
-      </StoreProvider>
+
     );
   };
   
